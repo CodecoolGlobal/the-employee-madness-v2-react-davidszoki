ea305711af77e5e6b9f3e97450ff32bd0deb61f7
davidszoki <davidszokoloczi@gmail.com>
2023-08-18 16:20:50 +0200

    add equipment shema

--- server/db/employee.model.js
+++ server/db/employee.model.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+const EquipmentSchema = new Schema({
+  name: String,
+  type: String,
+  amount: Number
+});
+
+const Equipment = mongoose.model("Equipment", EquipmentSchema);
+
+
 const EmployeeSchema = new Schema({
   name: String,
   level: String,
@@ -11,6 +20,7 @@ const EmployeeSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  equipment: [{type: mongoose.Schema.Types.ObjectId, ref: Equipment}]
 });
 
 module.exports = mongoose.model("Employee", EmployeeSchema);
